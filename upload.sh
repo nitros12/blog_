7f67cdeff5343a7fec4406b6ee2d0ecbb9ffb275
Ben Simms <ben@bensimms.moe>
2020-04-24 21:39:00 +0100

    Add some stuff

--- upload.sh
+++ upload.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-ssh -o ExitOnForwardFailure=yes -f -N -D 9999 dell0
+function get_unused_port() {
+  for port in $(seq 4444 65000);
+  do
+    echo -ne "\035" | telnet 127.0.0.1 "$port" > /dev/null 2>&1;
+    [ $? -eq 1 ] && echo "$port" && break;
+  done
+}
+port="$(get_unused_port)"
 
-curl -v -x socks5h://localhost:9999 --upload-file "{$(find public/ -type f | paste -s -d ',')}" -u "$CREDS" "$ADDR"
+echo "title; Ben Simms" > public/webindex.txt
+
+ssh -o ExitOnForwardFailure=yes -f -N -L "$port":www.lancaster.ac.uk:445 dell0
+smbclient -I localhost -p "$port" "$ADDR" -W "$WORKSPACE" -U "$CREDS" -c \
+  'prompt OFF; recurse ON; cd public_html; lcd public; mput *'
+
+pkill -P $$
